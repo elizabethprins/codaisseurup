6038e03b8cd9bebbabf4b2b302cc246fcfa62746
elizabethprins <elizabethprins@gmail.com>
2017-05-23 14:57:52 +0200

    adding many-to-many association between event and category

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,6 +7,7 @@ class EventsController < ApplicationController
   end
 
   def show
+    @categories = @event.categories
   end
 
   def new
@@ -40,6 +41,6 @@ class EventsController < ApplicationController
     end
 
     def event_params
-      params.require(:event).permit(:name, :description, :location, :price, :capacity, :includes_food, :includes_drinks, :starts_at, :ends_at, :active, :user)
+      params.require(:event).permit(:name, :description, :location, :price, :capacity, :includes_food, :includes_drinks, :starts_at, :ends_at, :active, :user, category_ids: [])
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.destroy_all
+Category.destroy_all
+
+daytime = Category.create!(name: "Daytime")
+outside = Category.create!(name: "Outside")
 
 elizabeth = User.create!(email: "elizabeth@codaisseurup.com", password: "abcd1234")
 
-event = Event.create!(name: "Hangout", description: "Normcore meh put a bird on it man braid mumblecore, locavore synth swag kogi helvetica ugh health goth twee raw denim readymade. Freegan yuccie everyday carry tote bag you probably haven't heard of them godard. Meditation tattooed lumbersexual street art truffaut pour-over, kale chips pitchfork. Readymade snackwave man bun, photo booth tilde wolf occupy fashion axe VHS authentic PBR&B typewriter. Vape everyday carry meditation try-hard hammock unicorn.", location: "The Spot", price: 5.5, capacity: 100, includes_food: true, includes_drinks: true, active: true, user: elizabeth)
+event = Event.create!(name: "Hangout", description: "Normcore meh put a bird on it man braid mumblecore, locavore synth swag kogi helvetica ugh health goth twee raw denim readymade. Freegan yuccie everyday carry tote bag you probably haven't heard of them godard. Meditation tattooed lumbersexual street art truffaut pour-over, kale chips pitchfork. Readymade snackwave man bun, photo booth tilde wolf occupy fashion axe VHS authentic PBR&B typewriter. Vape everyday carry meditation try-hard hammock unicorn.", location: "The Spot", price: 5.5, capacity: 100, includes_food: true, includes_drinks: true, active: true, user: elizabeth, categories: [daytime, outside])

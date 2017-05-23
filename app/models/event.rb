0359805813618b145cc55cf22039a660c621f35d
elizabethprins <elizabethprins@gmail.com>
2017-05-23 14:40:05 +0200

    add event views

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,4 @@ class Event < ApplicationRecord
 
   validates :name, presence: true
   validates :description, presence: true, length: {maximum: 500}
-  validates :starts_at, presence: true, on_or_after: -> { Time.now }
-  validates :ends_at, presence: true, on_or_after: -> {:starts_at}
 end

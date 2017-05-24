4d72f3049d7e6b7b8e7a1924b4f001aec61a8529
elizabethprins <elizabethprins@gmail.com>
2017-05-24 11:37:06 +0200

    added profile new template

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   has_one :profile
 
   def has_profile?
-    profile.present? && !profile.id.nil?
+    profile.present? && profile.persisted?
   end
 
   def full_name

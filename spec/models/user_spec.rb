58534671afe0ef4ecd255261e906942c7a56858f
elizabethprins <elizabethprins@gmail.com>
2017-05-26 11:17:54 +0200

    through association: registrations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -22,8 +22,19 @@ RSpec.describe User, type: :model do
     it { should have_one(:profile) }
   end
 
-  describe "association with profile" do
-    expect { user.destroy }.to change(Profile, :count).by(-1)
-  end
+#  describe "association with profile" do
+#    expect { user.destroy }.to change(Profile, :count).by(-1)
+#  end
 
+  describe "association with registration" do
+    let(:guest_user)   { create :user, email: "guest@user.com" }
+    let(:host_user)    { create :user, email: "host@user.com" }
+
+    let!(:event)        {create :event, user: host_user }
+    let!(:registration) { create :registration, event: event, user: guest_user }
+
+    it "has registrations" do
+      expect(guest_user.registered_events).to include(event)
+    end
+  end
 end
